c76b0b8620f30436793f2ab4b66f3e3d033c76e2
Tim Beals <tim.beals@gmail.com>
2017-03-08 12:01:46 -0800

    initial app setup with navigation controller and root view controller

---
+++ timmy_bea/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  timmy_bea
+//
+//  Created by Tim Beals on 2017-03-08.
+//  Copyright © 2017 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.gray
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
