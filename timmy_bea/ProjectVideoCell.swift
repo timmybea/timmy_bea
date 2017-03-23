efdc77ae5df1922024d91907399719e5474af137
Tim Beals <tim.beals@gmail.com>
2017-03-23 10:54:00 -0700

    create custom collectionViewCell class in helpers.swift. Create project datamodel struct

---
+++ timmy_bea/ProjectVideoCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProjectVideoCell.swift
+//  timmy_bea
+//
+//  Created by Tim Beals on 2017-03-23.
+//  Copyright © 2017 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class ProjectVideoCell: UICollectionViewCell {
+    
+}
