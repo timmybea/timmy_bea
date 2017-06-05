7a4226272a293cfc1f1674865ba7e1624da29f98
Tim Beals <tim.beals@gmail.com>
2017-06-05 10:17:19 -0700

    create borrowmatic projectmodel

--- timmy_bea/Project.swift
+++ timmy_bea/Project.swift
@@ -27,8 +27,10 @@ struct Project {
         let share = Project(title: "Share", shortDescription: "A food sharing social network", longDescription: "Share is a social networking platform that aims to mitigate food waste. Users post and request food items within their chosen radius and connect via a chat messenger.", frameWorks: "Firebase, JSQMessenger, CLLocation, WSTags, UIImagePicker", languages: "Swift", dateCompleted: "January 2017", videoThumbnailName: "Share_logo", videoURL: "https://firebasestorage.googleapis.com/v0/b/timmybea-3ba58.appspot.com/o/Share.mp4?alt=media&token=e02d4f65-d19f-4970-85a6-1701f5592cd1", gitHubURL: "https://github.com/suvan92/FinalProject")
         
         let meTube = Project(title: "MeTube", shortDescription: "A demonstration app for practising custom UI Design", longDescription: "MeTube is an attempt to recreate the YouTube app entirely without the use of storyboards. It has the look and feel of the original interface, loads videos and provides playback options that YouTube users are familiar with.", frameWorks: "NSURLSessions, NSCache, AVFoundation", languages: "Swift", dateCompleted: "February 2017", videoThumbnailName: "MeTube", videoURL: "https://firebasestorage.googleapis.com/v0/b/timmybea-3ba58.appspot.com/o/MeTube.mp4?alt=media&token=a1d6a2ba-c749-4d32-8deb-7ea845cdc1ea", gitHubURL: "https://github.com/timmybea/youtubeHomePage")
+        
+        let borrowmatic = Project(title: "Borrowmatic", shortDescription: "A CoreData demonstration app", longDescription: "A demonstration app that uses CoreData to persist person and borrowed item managed objects. Features: Fetched results controller handles tableview updates. Sort results by date (sort descriptor) and by person (section name key).", frameWorks: "CoreData, GLCalendarView, MLPAutoCompleteTextField", languages: "Swift", dateCompleted: "May 2017", videoThumbnailName: "Borrowmatic_logo", videoURL: "https://firebasestorage.googleapis.com/v0/b/timmybea-3ba58.appspot.com/o/Borrowmatic.mp4?alt=media&token=9dee3689-ffae-4266-9de6-b6b274814980", gitHubURL: "https://github.com/timmybea/CoreDataExpert")
             
-            return [meTube, share, gifter]
+            return [borrowmatic, meTube, share, gifter]
     }
 }
 
